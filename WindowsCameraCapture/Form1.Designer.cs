61171010419284a4355d7dfec912ae70ca665b77
JessieChang09 <1581755776@qq.com>
2018-08-20 10:20:41 +0800

    First commit
    This is the first commit.

---
+++ WindowsCameraCapture/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsCameraCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(4, 249);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(124, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Camera Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(134, 249);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(135, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Camera Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Height";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(319, 254);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(41, 12);
+            this.labelHeight.TabIndex = 4;
+            this.labelHeight.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Width";
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(399, 254);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(41, 12);
+            this.labelWidth.TabIndex = 6;
+            this.labelWidth.Text = "label2";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(330, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(446, 249);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 273);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Camera capture sample program";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsCameraCapture/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+using BeautoRover;
+
+namespace WindowsCameraCapture
+{
+    public partial class Form1 : Form
+    {
+        BeautoRoverlib br = new BeautoRoverlib();
+        private CvCapture capture;
+
+        public object COLOR_BGR2HSV { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            capture = CvCapture.FromCamera(1);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Setting framewidth and FrameHeight. In this case, framewidht is 320, and frameheight is 240.
+            Cv.SetCaptureProperty(capture, CaptureProperty.FrameWidth, 320);
+            Cv.SetCaptureProperty(capture, CaptureProperty.FrameHeight, 240);
+            textBox1.Text = br.OpenCOMPort("COM3");
+
+            // タイマーをスタート. Timer starts.
+            timer1.Start();
+            // 停止ボタンを使えるようにする. Enable stop button.
+            buttonStop.Enabled = true;
+            // 開始ボタンを使えないようにする Disable start button.
+            buttonStart.Enabled = false;
+        }
+        //private int ConvertToBmp(CvColor center)
+        //{
+        //    if (center.R < 70 && center.B < 70 && center.G > 80)
+        //    {
+        //        return 1;
+        //    }
+        //    else
+        //    {
+        //        return 0;
+        //    }
+        //}
+       
+        private int detect(IplImage ipl2, int a ,int b)
+        {
+            int sum = 0;
+            for (int y = 0; y < ipl2.Height; y++)
+            {
+                for (int x = ipl2.Width/7*a; x < ipl2.Width / 7 * b; x++)
+                {
+                    CvColor c = ipl2[y, x];
+
+                    // Red color extraction
+                    // If the pixel is red-like, the image is white, else black.
+                    if (c.R>25 && c.R < 50 && c.B>5 && c.B < 50 && c.G>70 && c.G <180)
+                    {
+                        sum++;
+                    }
+                }
+            }
+            return (sum-40>0?1:0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // キャプチャの開始. Capture starts.
+            IplImage ipl1 = capture.QueryFrame();
+            IplImage ipl2 = null;
+            Cv.CvtColor(ipl1, ipl2, ColorConversion.BgrToHsv);
+            // 取得したカメラ画像の高さと幅を取得し、labelに表示. Height and width of camera are shown in label.
+            labelWidth.Text = capture.FrameWidth.ToString();
+            labelHeight.Text = capture.FrameHeight.ToString();
+
+            if (ipl1 != null)
+            {
+                // pictureBoxに取得した画像を表示. Show the captured image.
+                pictureBox1.Image = ipl1.ToBitmap();
+                // メモリリークが発生するらしいので
+                // プログラムが動的に確保したメモリ領域のうち、
+                // 不要になった領域を定期的に自動解放する
+                if (GC.GetTotalMemory(false) > 600000)
+                {
+                    GC.Collect();
+                }
+
+                // Image processing should be written from here.
+
+                // Extract red color
+
+                CvColor center = ipl2[120, 160];
+                CvColor left =  ipl2[120, 0];
+                CvColor right = ipl2[119, 319];
+                int centerInt = detect(ipl2,1,6);
+                int leftInt = detect(ipl2,0,1);
+                int rightInt = detect(ipl2,6,7);
+
+                for (int y = 0; y < ipl1.Height; y++)
+                {
+                    for (int x = 0; x < ipl1.Width; x++)
+                    {
+                        CvColor c = ipl1[y, x];
+                        // Red color extraction
+                        // If the pixel is red-like, the image is white, else black.
+                        if (c.R > 25 && c.R < 50 && c.B > 5 && c.B < 50 && c.G > 70 && c.G > 80)
+                        {
+                            ipl1[y, x] = new CvColor()
+                            {
+                                B = 255,
+                                G = 255,
+                                R = 255,
+                            };
+                        }
+                        else
+                        {
+                            ipl1[y, x] = new CvColor()
+                            {
+                                // Red color extraction
+                                B = 0,
+                                G = 0,
+                                R = 0,
+                            };
+
+                        }
+                    }
+                }
+                // Show the image to picturebox2.
+                pictureBox2.Image = ipl1.ToBitmap();
+                //control the car
+
+                if (centerInt == 1 && leftInt == 0 && rightInt == 0)
+                {
+                    textBox1.Text = br.Forward();
+                } 
+                else if (leftInt == 1 && rightInt==0)
+                {
+                    textBox1.Text = br.TurnLeft();
+                }
+                else if(rightInt==1 && leftInt==0)
+                {
+                    textBox1.Text = br.TurnRight();
+                }
+                else if (leftInt == 1 && rightInt == 1)
+                {
+                    textBox1.Text = br.Back();
+                }
+                else
+                {
+                    textBox1.Text = br.Stop();
+                }
+
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            // タイマーをストップ. Timer stop.
+            timer1.Stop();
+            // 開始ボタンを使えるようにする. Enable start button.
+            buttonStart.Enabled = true;
+            // 停止ボタンを使えないようにする. Disable stop button.
+            buttonStop.Enabled = false;
+            textBox1.Text = br.Close();
+
+        }
+    }
+}
